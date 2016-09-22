114072e2c509b0f09170ddb4af1c38ac30279dfe
Tiago Martins <tm@binaryedge.io>
2016-09-22 10:44:42 +0100

    Decrease logs and use slice instead of Magnet object

--- web/models/torrent.ex
+++ web/models/torrent.ex
@@ -25,7 +25,7 @@ defmodule Magnetissimo.Torrent do
     struct
     |> cast(params, [:infohash, :name, :magnet, :leechers, :seeders, :source, :filesize])
     |> validate_required([:infohash, :name, :magnet, :source])
-#    |> unique_constraint(:infohash)
+    |> unique_constraint(:infohash)
   end
 
   def order_by_name(query) do
@@ -49,12 +49,29 @@ defmodule Magnetissimo.Torrent do
   end
 
   def save_torrent(torrent) do
-    Logger.info "Got new torrent!!!! #{torrent.magnet}"
-    magnet_decoded = Magnet.decode(torrent.magnet) |> Enum.into(%Magnet{})
-    infohash = magnet_decoded.info_hash
-    Logger.info "MAGNET DECODE #{infohash}"
+    #Logger.info "Got new torrent!!!! #{torrent.magnet}"
 
-    changeset = Torrent.changeset(%Torrent{}, torrent)
+
+    #magnet_decoded = Magnet.decode(torrent.magnet) |> Enum.into(%Magnet{})
+    #magnet_decoded = Magnet.decode(torrent.magnet)
+    #magnet_enum = magnet_decoded |> Enum.into(%Magnet{})
+
+    #Logger.info "enum #{magnet_enum}"
+    #Logger.info "tuple #{magnet_decoded}"
+    #infohash = magnet_enum.info_hash
+
+    #Logger.info "LALALALALALAL"
+    #first_infohash = List.first(infohash)
+
+    #infohash_split = List.last(String.split(first_infohash, ":"))
+    #Logger.info "Teste #{infohash_split}"
+
+    #changeset = Torrent.changeset(%Torrent{infohash: String.downcase(infohash_split)}, torrent)
+
+    magnet_position = :binary.match torrent.magnet, "magnet:?xt=urn:btih:"
+    infohash = String.slice(torrent.magnet, magnet_position |> elem(1), 40)
+
+    changeset = Torrent.changeset(%Torrent{infohash: String.downcase(infohash)}, torrent)
     case Repo.insert(changeset) do
       {:ok, _torrent} ->
         Logger.info "★★★ - Torrent saved to database: #{torrent.name}"
