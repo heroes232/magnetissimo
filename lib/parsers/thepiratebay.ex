ec99126a4366ad81620bbdbb72520af23550e3f4
Tiago Martins <tm@binaryedge.io>
2016-09-22 19:19:06 +0100

    Fix Piratebay page bug

--- lib/parsers/thepiratebay.ex
+++ lib/parsers/thepiratebay.ex
@@ -10,8 +10,7 @@ defmodule Magnetissimo.Parsers.ThePirateBay do
   end
 
   def paginated_links(_) do
-    for i <- 1..6, j <- 1..1, do: "https://thepiratebay.org/browse/#{i}00/#{j}/3"
-    # for i <- 1..6, j <- 1..50, do: "https://thepiratebay.org/browse/#{i}00/#{j}/3"
+    for i <- 1..6, j <- 1..50, do: "https://thepiratebay.org/browse/#{i}00/10/#{j}"
   end
 
   def torrent_links(html_body) do
