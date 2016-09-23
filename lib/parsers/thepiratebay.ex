8d454fc7cd16838c49f414204c1233df009e339f
Tiago Martins <tm@binaryedge.io>
2016-09-23 14:34:21 +0100

    Fix pirate bay url

--- lib/parsers/thepiratebay.ex
+++ lib/parsers/thepiratebay.ex
@@ -10,7 +10,7 @@ defmodule Magnetissimo.Parsers.ThePirateBay do
   end
 
   def paginated_links(_) do
-    for i <- 1..6, j <- 1..50, do: "https://thepiratebay.org/browse/#{i}00/10/#{j}"
+    for i <- 1..6, j <- 0..50, do: "https://thepiratebay.org/browse/#{i}00/#{j}/3"
   end
 
   def torrent_links(html_body) do
